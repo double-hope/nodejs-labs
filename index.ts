5f5cafd820832dfc47094cba24ccf456f854537b
cyberpOOP <vlad0021135@gmail.com>
2023-02-20 20:03:28 +0200

    create unique links array for chukopop!

--- index.ts
+++ index.ts
@@ -9,16 +9,19 @@ const port = 3001;
 const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {
   response.end('Hello world!');
 });
-const articles: any[] = [];
 
 request(url, {json: true}, (err, res, body) => {
   if (err) { return console.log(err); }
   
   const $ = cheerio.load(body);
 
-  $('article').each((i, el) => {
-    console.log($(el).html());
+  let links:string[] = [];
+
+  $('article a').each((i, el) => {
+    links.push($(el).attr("href"));
   });
+  let unique = [... new Set(links)];
+  console.log(unique);
   
 });
 
