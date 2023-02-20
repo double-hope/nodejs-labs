90016add6e57858a0810c4880282e8df725927b7
double-hope <nadia.prohorchuk@gmail.com>
2023-02-20 19:47:19 +0200

    get all articles

--- index.ts
+++ index.ts
@@ -9,15 +9,17 @@ const port = 3001;
 const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {
   response.end('Hello world!');
 });
+const articles: any[] = [];
 
 request(url, {json: true}, (err, res, body) => {
   if (err) { return console.log(err); }
   
   const $ = cheerio.load(body);
-  console.log($);
+
+  $('article').each((i, el) => {
+    console.log($(el).html());
+  });
   
-
-
 });
 
 server.listen(port);
