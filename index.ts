0c64bebfe6d281630df9076093949dc6d6a0b1c7
cyberpOOP <vlad0021135@gmail.com>
2023-02-23 15:53:57 +0200

    add request handler | change app logic

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import * as http from 'http';
-import request from 'request';
-import * as cheerio from 'cheerio';
 import * as fs from 'fs';
+import {requestHandler} from "./handlers/request-handler";
 
 const page = 1;
 const url = `https://www.factcheck.org/page/${page}`
@@ -11,53 +10,14 @@ let links: string[] = [];
 
 const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {
     response.setHeader('Content-Type', 'text/html');
-    response.write('<h1>fhrjfk</h1>');
+    response.write('<h1></h1>');
+
+     requestHandler().then(res => console.log(res));
     
     response.end();
 });
 
-//setInterval(() => {
-    request(url, {json: true}, (err, res, body) => {
-        if (err) 
-            return console.log(err);
 
-        const $ = cheerio.load(body);
 
-        $('article a').each((i, el) => {
-            links.push($(el).attr('href'));
-        });
-
-        links = [...new Set(links)];
-
-        links.forEach((link, i) => {
-            request(link, {json:true}, (err, res, body)=>{
-                if(err)
-                    return console.log(err);
-
-                const $ = cheerio.load(body);
-                const name = $('.entry-title').text();
-                let text = '';
-
-                $('.entry-content p').each((i, el) => {
-                    text += $(el).text();
-                });
-
-                const article = {
-                    name,
-                    text,
-                }
-                
-                fs.mkdir(`./news/news№${i}`, {recursive: true} ,(err)=>{
-                    if(err) console.log(err);
-                });
-
-                fs.writeFile(`./news/news№${i}/${article.name}.json`, JSON.stringify(article), function (err){
-                    if(err) console.log(err);
-                })
-            });
-        })
-
-    });
-//}, 10);
 
 server.listen(port);
