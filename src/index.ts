4ae787084f575232a384d4489275b493f48bfbfd
cyberpOOP <vlad0021135@gmail.com>
2023-02-24 15:44:38 +0200

    all done|fix some crashes

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ const basePath = path.resolve();
 
 const server = http.createServer(async (request: http.IncomingMessage, response: http.ServerResponse) => {
     response.setHeader('Content-Type', 'text/html');
-    response.write('<h1>dd</h1>');
  
     requestHandler().then(res => {
         readHandler(basePath).then(async res => {
@@ -19,9 +18,24 @@ const server = http.createServer(async (request: http.IncomingMessage, response:
         });
     }).catch(err => console.log(err));
 
+
+    // don't ask!
+    response.write("<script>\n" +
+        "    function changeVisibility(i){\n" +
+        "        const text = document.getElementById(`text${i}`);\n" +
+        "        if(text.style.display ===\"none\")\n" +
+        "            text.style.display = \"block\";\n" +
+        "        else if(text.style.display === \"block\")\n" +
+        "            text.style.display = \"none\";\n" +
+        "\n" +
+        "\n" +
+        "    }\n" +
+        "</script>");
+
     (await function() {
-        news.map(data => {
-            response.write(`<h3>${data.name}</h3>`)
+        news.map((data, key) => {
+            response.write(`<h3 onclick="changeVisibility(${key});" style="cursor: pointer">${data.name}</h3>`)
+            response.write(`<p id="text${key}" style="display: none">${data.text}</p>`)
         })
     }) ()
 
