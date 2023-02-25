bc2ecd3de94ea04fcf0b1a1cff317f42a70545cd
double-hope <nadia.prohorchuk@gmail.com>
2023-02-25 22:34:11 +0200

    use sync methods

--- src/handlers/request-handler.ts
+++ src/handlers/request-handler.ts
@@ -8,53 +8,53 @@ const url = `https://www.factcheck.org/page/${page}`
 let links: any[] = [];
 
 const requestHandler = () => {
-  return new Promise((resolve, reject) => {
-      request(url, {json: true}, (err, res, body) => {
-          if (err)
-              return console.log(err);
+    return new Promise((resolve, reject) => {
+        request(url, {json: true}, (err, res, body) => {
+            if (err)
+                return console.log(err);
 
-          const $ = cheerio.load(body);
+            const $ = cheerio.load(body);
 
-          $('article a').each((i, el) => {
-              links.push($(el).attr('href'));
-          });
+            $('article a').each((i, el) => {
+                links.push($(el).attr('href'));
+            });
 
-          links = [...new Set(links)];
+            links = [...new Set(links)];
 
-          links.forEach((link, i) => {
-              request(link, {json:true}, (err, res, body) => {
-                  if(err)
-                      return console.log(err);
+            links.forEach((link, key) => {
+                request(link, {json:true}, (err, res, body) => {
+                    if(err)
+                        return console.log(err);
 
-                  const $ = cheerio.load(body);
-                  const name = $('.entry-title').text();
-                  let text = '';
+                    const $ = cheerio.load(body);
+                    const name = $('.entry-title').text();
+                    let text = '';
 
-                  $('.entry-content p').each((i, el) => {
-                      text += $(el).text();
-                  });
+                    $('.entry-content p').each((key, elem) => {
+                        text += $(elem).text();
+                    });
 
-                  const article = {
-                      name,
-                      text,
-                  }
+                    const article = {
+                        name,
+                        text,
+                    }
 
-                  // ХЕРНЯ, ПЕРЕРОБИТИ!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    try {
+                        fs.writeFileSync(`./news/news№${key}/${article.name}.json`, JSON.stringify(article));
+                    } catch(err) {
+                        reject(err);
+                    }
 
+                    try {
+                        fs.mkdirSync(`./news/news№${key}`, {recursive: true});
+                    } catch(err) {
+                        reject(err);
+                    }
+                    
+                    resolve('File saved correctly');
 
-                  // if(fs.existsSync('./news')) fs.rmSync('./news', { recursive: true, force: true });
-
-                  fs.mkdir(`./news/news№${i}`, {recursive: true} ,(err)=>{
-                      if(err)
-                          reject(err);
-                  });
-
-                  fs.writeFile(`./news/news№${i}/${article.name}.json`, JSON.stringify(article), function (err){
-                      if(err) reject(err);
-                      else resolve("File saved correctly");
-                  })
-              });
-          })
+                });
+            })
 
       });
   });
