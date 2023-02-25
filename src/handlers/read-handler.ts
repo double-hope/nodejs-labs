835f953118f5ea0aa58857d4a50ce2dabd55997d
double-hope <nadia.prohorchuk@gmail.com>
2023-02-25 22:34:43 +0200

    change variable name

--- src/handlers/read-handler.ts
+++ src/handlers/read-handler.ts
@@ -6,20 +6,19 @@ type Unpromise<T> = T extends Promise<infer U> ? U : never;
 
 const readHandler = (basePath: string) : Unpromise<Promise<Promise<string[]>>> => {
     return new Promise((resolve, reject) => {
-        const length = fs.readdirSync(`${basePath}/news`).length;
 
-        for(let i = 0; i < length; i++){
-            news.push(fs.readdirSync(`${basePath}/news/news№${i}`).toString());
+        fs.readdirSync(`${basePath}/news`).forEach((elem, key) => {
+            news.push(fs.readdirSync(`${basePath}/news/news№${key}`).toString());
 
             promises.push(new Promise((resolve, reject) => {
-                const file = fs.readFileSync(`${basePath}/news/news№${i}/${news[i]}`, 'utf8');                
+                const file = fs.readFileSync(`${basePath}/news/news№${key}/${news[key]}`, 'utf8');                
                 !!file ? resolve(file): reject('Error while reading file');
             }));
-        }
+        })
         
         Promise.all(promises).then((res)=>{
             resolve(res);
-        });
+        }).catch(err => reject(err));
     });
 }
 
